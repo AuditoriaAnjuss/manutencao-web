5870f05ff0f2aa87d6366e0d1fa17c5bf1be5a4d
AuditoriaAnjuss <136639198+AuditoriaAnjuss@users.noreply.github.com>
2023-09-27 12:19:58 -0300

    teste

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
+const express = require('express');
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({message: "Hello from server!"});
-});
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Aqui você pode adicionar a lógica de validação do login e senha
+  // Por exemplo, você pode verificar se o username e password correspondem a um usuário válido no banco de dados
+  console.log('Olá mundo!')
+  if (username === 'admin' && password === 'password') {
+    res.status(200).json({ message: 'Login successful' });
+  } else {
+    res.status(401).json({ message: 'Invalid username or password' });
+  }
+});
 
 module.exports = app;
